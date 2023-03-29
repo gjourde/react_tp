4bccef17c0d586a7ec2bfc5c71accde26e212a9e
Guillaume JOURDE <guillaume.jourde@gaea21.org>
2023-03-29 15:26:46 +0200

    rien

--- assets/app.jsx
+++ assets/app.jsx
@@ -15,6 +15,15 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import Main from "./components/Main";
 
+const priceList = [
+    { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
+    { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
+    { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
+    { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
+    { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
+    { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
+];
+
 const appRoot = document.getElementById('app-root');
 const root = createRoot(appRoot); // createRoot(container!) if you use TypeScript
 root.render(<Main />);
